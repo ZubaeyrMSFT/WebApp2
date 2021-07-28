1491d154f293a5553779b2e0d8d3ee32789726b1
Zubairuddin Mohammed <zmohammed@microsoft.com>
2021-07-29 00:39:52 +0530

    Add project files.

---
+++ WebAPI/Controllers/ApplicationController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApplicationController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ApplicationController> _logger;
+
+        public ApplicationController(ILogger<ApplicationController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [Route("/")]
+        public String getWelcomeMessage()
+        {
+            return "Welcome to WebApp 1.0";
+        }
+
+
+        [Route("/weatherinfo")]
+        public IEnumerable<WeatherForecast> getWeatherInfo()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [Route("/error")]
+        public String getErrorMessage()
+        {
+            return "Resource Not Found!";
+        }
+
+    }
+}
